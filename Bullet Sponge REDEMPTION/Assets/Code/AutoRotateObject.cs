0f5888270890e67ce7891c8f86c33a667e81cee3
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-10-26 09:20:52 +0100

    Init  commit

---
+++ Bullet Sponge REDEMPTION/Assets/Code/AutoRotateObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotateObject : MonoBehaviour {
+    public float speed;
+    [Range(-1, 1)]
+    public int x, y, z;
+
+    private void FixedUpdate() {
+        Vector3 newRot = new Vector3(x, y, z) * speed * Time.deltaTime;
+        transform.Rotate(newRot);
+    }
+}
