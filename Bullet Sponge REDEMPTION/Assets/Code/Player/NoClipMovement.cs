dea7500ec65a32caa9aa232d3a3baadd75cda037
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-11-13 11:11:06 +0100

    Ingame unity scene cam noclip

--- Bullet Sponge REDEMPTION/Assets/Code/Player/NoClipMovement.cs
+++ Bullet Sponge REDEMPTION/Assets/Code/Player/NoClipMovement.cs
@@ -3,21 +3,127 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NoClipMovement : MonoBehaviour {
-    public float speed, turnSpeed;
+    [Range(1, 20)]
+    public int speed;
+    public float turnSpeed;
+
+    public int fontSize;
+    public float outline;
+    [Space]
+    public float width;
+    public float height;
+    public float x, y;
+
+    [Space]
+    public float waitForFade;
+    public TextAnchor anchor;
+
+    string text;
+    Coroutine lastCoroutine = null;
+    float hor, vert, mouseX, mouseY, upDown;
+    bool gui = false, buttonDown = false;
+
+    private void Start() {
+        speed = PlayerPrefs.GetInt("NoClip", 10);
+    }
+
+    private void Update() {
+        buttonDown = Input.GetMouseButton(1);
+        if (buttonDown) {
+            hor = Input.GetAxis("Horizontal");
+            vert = Input.GetAxis("Vertical");
+            mouseY = -Input.GetAxis("Mouse Y");
+            mouseX = Input.GetAxis("Mouse X");
+            upDown = Input.GetAxis("NC_UpDown");
+
+            float f = Input.GetAxis("Mouse ScrollWheel");
+            if (f!=0) {
+                speed += (int)Mathf.Sign(f);
+                speed = Mathf.Clamp(speed, 1, 20);
+                gui = true;
+                text = (speed * .1f).ToString();
+                PlayerPrefs.SetInt("NoClip", speed);
+                UpdateGUI();
+            }
+        }
+    }
+
+    int GetSprint() {
+        int i = 1;
+        if (Input.GetButton("Sprint")) {
+            i = 2;
+        }
+        return i;
+    }
 
     private void FixedUpdate() {
-        float hor = Input.GetAxis("Horizontal");
-        float vert = Input.GetAxis("Vertical");
+        if (buttonDown) {
+            Vector3 newPos = new Vector3(hor, upDown, vert) * speed * Time.deltaTime;
+            newPos *= GetSprint();
+            transform.Translate(newPos);
 
-        Vector3 newPos = new Vector3(hor, 0, vert) * speed * Time.deltaTime;
-        transform.Translate(newPos);
+            Vector3 newRot = new Vector3(mouseY, mouseX) * turnSpeed * Time.deltaTime;
+            newRot += transform.rotation.eulerAngles;
+            transform.rotation = Quaternion.Euler(newRot);
+        }
+    }
 
-        float mouseY = -Input.GetAxis("Mouse Y");
-        float mouseX = Input.GetAxis("Mouse X");
-        Vector3 newRot = new Vector3(mouseY, mouseX) * turnSpeed * Time.deltaTime;
+    void UpdateGUI() {
+        if (lastCoroutine != null) {
+            StopCoroutine(lastCoroutine);
+        }
+        lastCoroutine = StartCoroutine(IeUpdateGUI());
+    }
 
-        newRot += transform.rotation.eulerAngles;
+    IEnumerator IeUpdateGUI() {
+        yield return new WaitForSeconds(waitForFade);
+        gui = false;
+    }
 
-        transform.rotation = Quaternion.Euler(newRot);
+    private void OnGUI() {
+        if (gui) {
+            GUIStyle style = new GUIStyle();
+            style.fontSize = fontSize;
+            style.alignment = anchor;
+            style.fixedHeight = height;
+            style.fixedWidth = width;
+            style.normal.textColor = Color.white;
+
+            float w = Screen.width / 2;
+            float h = Screen.height / 2;
+            Rect rect = new Rect(w - x, h - y, 0f, 0f);
+
+            DrawOutline(rect, text, style, Color.black, Color.white, outline);
+        }
+    }
+
+    public void DrawOutline(Rect rect, string text, GUIStyle style, Color outColor, Color inColor, float size) {
+        float halfSize = size * 0.5F;
+        GUIStyle backupStyle = new GUIStyle(style);
+        Color backupColor = GUI.color;
+
+        backupStyle.normal.textColor = outColor;
+        GUI.color = outColor;
+
+        rect.x -= halfSize;
+        GUI.Label(rect, text, style);
+
+        rect.x += size;
+        GUI.Label(rect, text, style);
+
+        rect.x -= halfSize;
+        rect.y -= halfSize;
+        GUI.Label(rect, text, style);
+
+        rect.y += size;
+        GUI.Label(rect, text, style);
+
+        rect.y -= halfSize;
+        style.normal.textColor = inColor;
+        GUI.skin.box = style;
+        GUI.color = backupColor;
+        GUI.Label(rect, text, style);
+
+        style = backupStyle;
     }
 }
